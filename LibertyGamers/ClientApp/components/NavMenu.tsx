bca127076be7f39b47b5854219f06dbc1a45b9a3
Jon Byrd <jonathan.byrd.2@gmail.com>
2017-10-20 14:30:40 -0400

    Working on the active/hover nav button states

--- LibertyGamers/ClientApp/components/NavMenu.tsx
+++ LibertyGamers/ClientApp/components/NavMenu.tsx
@@ -9,21 +9,38 @@ import {
 } from 'react-bootstrap';
 import { NavLink, Link } from 'react-router-dom';
 
-export class NavMenu extends React.Component<{}, {}> {
-    public render() {
-        return <div className='lg-navbar'>
-            <Navbar>
-                <Navbar.Header>
-                    <Navbar.Brand>
-                        <a href="/" className="lg-title">Liberty Gamers</a>
-                    </Navbar.Brand>
-                </Navbar.Header>
-                <Nav>
-                    <NavItem eventKey={1} href={ '/' }>News</NavItem>
-                    <NavItem eventKey={2} href={ '/counter' }>Counter</NavItem>
-                </Nav>
-            </Navbar>
-            </div>;
+interface NavProps {}
+interface NavState {
+    key: number
+}
+
+function handleSelected(key: number) {
+    
+}
+
+export class NavMenu extends React.Component<NavProps, NavState> {
+    public constructor(props: NavProps) {
+        super(props);
+        this.state = {
+            key: 1
+        }
+    }
+    public render() {        
+        return (
+            <div className='lg-navbar'>
+                <Navbar>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <a href="/" className="lg-title">Liberty Gamers</a>
+                        </Navbar.Brand>
+                    </Navbar.Header>
+                    <Nav activeKey={ this.state.key } onSelect={this.setState}>
+                        <NavItem eventKey={1} href={ '/' }>News</NavItem>
+                        <NavItem eventKey={2} href={ '/counter' }>Counter</NavItem>
+                    </Nav>
+                </Navbar>
+            </div>
+        );
         //         <div className='navbar navbar-inverse'>
         //         <div className='navbar-header'>
         //             <button type='button' className='navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse'>
