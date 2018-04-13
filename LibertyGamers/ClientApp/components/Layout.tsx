1a79907e675fd73ed08d20486932724b61038a27
Jon Byrd <jonathan.byrd.2@gmail.com>
2018-04-13 17:46:06 -0400

    WIP

--- LibertyGamers/ClientApp/components/Layout.tsx
+++ LibertyGamers/ClientApp/components/Layout.tsx
@@ -1,16 +1,21 @@
 import * as React from 'react';
 import { Jumbotron } from './Jumbotron';
 import { NavMenu } from './NavMenu';
+import { WorldClock } from './WorldClock';
 
 export class Layout extends React.Component<{}, {}> {
     public render() {
         return <div className="lg-page">            
-                <NavMenu />
-                <Jumbotron />
+            <NavMenu />
+            <Jumbotron />
+            <div className='lg-content'>
                 <div className='lg-body'>
                     { this.props.children }
                 </div>
-                <div className="lg-sidebar"></div>
+                <div className="lg-sidebar">
+                    <WorldClock />
+                </div>
+            </div>            
         </div>;
     }
 }
--- LibertyGamers/ClientApp/components/WorldClock.tsx
+++ LibertyGamers/ClientApp/components/WorldClock.tsx
@@ -1,22 +1,51 @@
 import * as React from "react";
+import * as jstz from "jstimezonedetect";
+import * as moment from "moment";
 
-interface WorldClockProps {
-    timerId: number
-}
+interface WorldClockProps {}
 interface WorldClockState {
-    date: any
+    time: any
+    timeZone: string
 }
 
 export class WorldClock extends React.Component<WorldClockProps, WorldClockState> {
-    state = {
-        date: (new Date()).toLocaleTimeString
+    state ={
+        time: new Date(),
+        timeZone: jstz.determine().name()
+    };
+    private updateDate(): void {
+        setInterval(() => {this.setState({
+            time: new Date()
+        })},
+        60000);
     }
-    public componentDidMount() {
-        
-    }
-    public render() {
-        return <div className="lg-worldClock">
 
+    public render() {
+        this.updateDate();
+        
+        return <div className="lg-worldClock">
+            <label>UTC: </label>
+            {this.state.time.toUTCString()}
+            <br />
+            <label>Local: </label>
+            {this.state.time.toLocaleTimeString()}
+            
         </div>
+    }
+}
+
+interface ClockProps{
+    timezone: string,
+    time: any,
+    offset: number
+}
+
+class Clock extends React.Component<{}, {}> {
+    public render() {
+        return (
+            <div className="lg-clock">
+                
+            </div>
+        );
     }
 }
